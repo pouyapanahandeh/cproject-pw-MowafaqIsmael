fb154a0210f409f97fe583d6c56b861deb64d18b
pouyapanahandeh <panahandehpooya@gmail.com>
2022-07-18 16:50:43 +0200

    card one has been added to the index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,18 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="full-width">
+    <IntroductionMIVue />
+    <!-- <MISponsor /> -->
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import IntroductionMIVue from "src/components/MainPageComponents/IntroductionMI.vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  components: {
+    IntroductionMIVue,
+  },
+  name: "IndexPage",
+});
 </script>

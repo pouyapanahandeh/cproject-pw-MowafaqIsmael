050d5e5521dd144633db2abdde9be025133da009
pouyapanahandeh <panahandehpooya@gmail.com>
2022-07-18 19:21:34 +0200

    index page has been populated with new components

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,28 @@
 <template>
   <q-page class="full-width">
     <IntroductionMIVue />
-    <!-- <MISponsor /> -->
+    <MowafaqGallary />
+    <MIServices />
+    <CustomerGallary />
+    <MISponsor />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import IntroductionMIVue from "src/components/MainPageComponents/IntroductionMI.vue";
+import MowafaqGallary from "src/components/MainPageComponents/MowafaqGallary.vue";
+import MIServices from "src/components/MainPageComponents/MIServices.vue";
+import CustomerGallary from "src/components/MainPageComponents/CustomerGallary.vue";
+import MISponsor from "src/components/MainPageComponents/MISponsor.vue";
 
 export default defineComponent({
   components: {
     IntroductionMIVue,
+    MowafaqGallary,
+    MIServices,
+    CustomerGallary,
+    MISponsor,
   },
   name: "IndexPage",
 });

23c49cf1f0fd3a6ae8783e014b65ac10320661e4
pouyapanahandeh <panahandehpooya@gmail.com>
2022-07-21 18:15:24 +0200

    coming soon index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,28 +1,31 @@
 <template>
   <q-page class="full-width">
-    <IntroductionMIVue />
+    <!-- Coming soon -->
+    <N7TTeamCS />
+    <!-- <IntroductionMIVue />
     <MowafaqGallary />
     <MIServices />
     <CustomerGallary />
-    <MISponsor />
+    <MISponsor /> -->
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import IntroductionMIVue from "src/components/MainPageComponents/IntroductionMI.vue";
-import MowafaqGallary from "src/components/MainPageComponents/MowafaqGallary.vue";
-import MIServices from "src/components/MainPageComponents/MIServices.vue";
-import CustomerGallary from "src/components/MainPageComponents/CustomerGallary.vue";
-import MISponsor from "src/components/MainPageComponents/MISponsor.vue";
-
+// import IntroductionMIVue from "src/components/MainPageComponents/IntroductionMI.vue";
+// import MowafaqGallary from "src/components/MainPageComponents/MowafaqGallary.vue";
+// import MIServices from "src/components/MainPageComponents/MIServices.vue";
+// import CustomerGallary from "src/components/MainPageComponents/CustomerGallary.vue";
+// import MISponsor from "src/components/MainPageComponents/MISponsor.vue";
+import N7TTeamCS from "src/components/ComingSoon/N7TTeamCS.vue";
 export default defineComponent({
   components: {
-    IntroductionMIVue,
-    MowafaqGallary,
-    MIServices,
-    CustomerGallary,
-    MISponsor,
+    // IntroductionMIVue,
+    // MowafaqGallary,
+    // MIServices,
+    // CustomerGallary,
+    // MISponsor,
+    N7TTeamCS,
   },
   name: "IndexPage",
 });

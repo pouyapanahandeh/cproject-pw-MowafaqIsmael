c0526cde1e6e1769f9f35e1dff8ec2b4b43562f0
pouyapanahandeh <panahandehpooya@gmail.com>
2022-07-24 17:46:59 +0200

    services and media has been added

--- src/components/MainPageComponents/MIServices.vue
+++ src/components/MainPageComponents/MIServices.vue
@@ -1,58 +1,155 @@
 <template>
-  <div class="row">
-    <div class="col-6 col-md-6 col-sm-6 col-xs-12">
-      <div class="row">
-        <div class="col-6 col-md-6 col-sm-6 col-xs-12">
-          <q-card class="my-card q-ma-md">
-            <q-img src="../../assets/imgs/MI-4.jpeg">
-              <div class="absolute-bottom text-subtitle2 text-center">Diet</div>
-            </q-img>
-          </q-card>
-        </div>
-        <div class="col-6 col-md-6 col-sm-6 col-xs-12">
-          <q-card class="my-card q-ma-md">
-            <q-img src="../../assets/imgs/MI-4.jpeg">
-              <div class="absolute-bottom text-subtitle2 text-center">
-                workout
-              </div>
-            </q-img>
-          </q-card>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-6 col-md-6 col-sm-6 col-xs-12">
-          <q-card class="my-card q-ma-md">
-            <q-img src="../../assets/imgs/MI-4.jpeg">
-              <div class="absolute-full text-subtitle2 flex flex-center">
-                Nutrition and supplements
-              </div>
-            </q-img>
-          </q-card>
-        </div>
-        <div class="col-6 col-md-6 col-sm-6 col-xs-12">
-          <q-card class="my-card q-ma-md">
-            <q-img src="../../assets/imgs/MI-5.jpeg">
-              <div class="absolute-full text-subtitle2 flex flex-center">
-                Competting
-              </div>
-            </q-img>
-          </q-card>
-        </div>
+  <div class="flex flex-center">
+    <div class="row q-my-md">
+      <span class="text-white text-h4 tag-line-text">Service plan</span>
+    </div>
+  </div>
+  <div class="row justify-around q-ma-lg">
+    <div class="col-6 col-md-5 col-sm-12 col-xs-12">
+      <div class="q-ma-md">
+        <q-card
+          class="my-card bg-primary text-white"
+          style="border: 1px solid white"
+        >
+          <q-img src="../../assets/imgs/MI-7.jpeg">
+            <div class="absolute-full tag-line-text text-h5 flex flex-center">
+              1 month plan
+            </div>
+          </q-img>
+
+          <q-list>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-avatar size="lg"> &#127857; </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="text-h6 tag-line-text"
+                  >Diet and nutrients plan</q-item-label
+                >
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-avatar size="lg"> &#127939;</q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="text-h6 tag-line-text"
+                  >workout plan</q-item-label
+                >
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar
+                ><q-avatar size="lg"> &#128232;</q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="text-h6 tag-line-text"
+                  >Email Support</q-item-label
+                >
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar
+                ><q-avatar size="lg"> &#128178; </q-avatar></q-item-section
+              >
+
+              <q-item-section>
+                <q-item-label class="text-h6 tag-line-text">100 $</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section class="flex flex-center">
+                <q-btn
+                  push
+                  class="bg-secondary text-white tag-line-text full-width"
+                  label="select plan"
+                  to="/get-program"
+                />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card>
       </div>
     </div>
-    <div class="col-6 col-md-6 col-sm-6 col-xs-12">
-      <q-img
-        src="../../assets/imgs/MI-4.jpeg"
-        alt="Mowafaq Ismael"
-        style="
-          background-size: cover;
-          background-repeat: no-repeat;
-          height: 100%;
-          width: 100%;
-        "
-      >
-        <div class="absolute-full text-subtitle2 flex flex-center">Caption</div>
-      </q-img>
+    <div class="col-6 col-md-5 col-sm-12 col-xs-12">
+      <div class="q-ma-md">
+        <q-card
+          class="my-card bg-primary text-white"
+          style="border: 1px solid white"
+        >
+          <q-img src="../../assets/imgs/MI-7.jpeg">
+            <div class="absolute-full tag-line-text text-h5 flex flex-center">
+              1 month plan
+            </div>
+          </q-img>
+
+          <q-list>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-avatar size="lg"> &#127857; </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="text-h6 tag-line-text"
+                  >Diet and nutrients plan</q-item-label
+                >
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-avatar size="lg"> &#127939;</q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="text-h6 tag-line-text"
+                  >workout plan</q-item-label
+                >
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar
+                ><q-avatar size="lg"> &#128242;</q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="text-h6 tag-line-text"
+                  >Instant online support</q-item-label
+                >
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable>
+              <q-item-section avatar
+                ><q-avatar size="lg"> &#128178; </q-avatar></q-item-section
+              >
+
+              <q-item-section>
+                <q-item-label class="text-h6 tag-line-text">200 $</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section class="flex flex-center">
+                <q-btn
+                  push
+                  class="bg-secondary text-white tag-line-text full-width"
+                  label="select plan"
+                  to="/get-program"
+                />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card>
+      </div>
     </div>
   </div>
 </template>

3c7c584bfb2aa8269abdb9b7415addc32b86e035
pouyapanahandeh <panahandehpooya@gmail.com>
2022-07-24 15:59:08 +0200

    Media page

---
+++ src/components/MainPageComponents/ApearInMedia.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="full-width bg-secondary">
+    <div class="flex flex-center">
+      <span class="text-h3 text-white tag-line-text q-my-md"> Appears on</span>
+    </div>
+    <div class="flex flex-center">
+      <div class="row q-my-md">
+        <q-img
+          src="../../assets/imgs/media/NPC-logo.png"
+          style="width: 200px"
+        ></q-img>
+      </div>
+    </div>
+  </div>
+</template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page class="full-width">
     <IntroductionMI1 />
+    <ApearInMedia />
     <MIServices />
     <CustomerGallary />
     <MISponsor />
@@ -10,6 +11,7 @@
 <script>
 import { defineComponent } from "vue";
 import IntroductionMI1 from "src/components/MainPageComponents/IntroductionMI1.vue";
+import ApearInMedia from "src/components/MainPageComponents/ApearInMedia.vue";
 import MIServices from "src/components/MainPageComponents/MIServices.vue";
 import CustomerGallary from "src/components/MainPageComponents/CustomerGallary.vue";
 import MISponsor from "src/components/MainPageComponents/MISponsor.vue";
@@ -17,6 +19,7 @@ import MISponsor from "src/components/MainPageComponents/MISponsor.vue";
 export default defineComponent({
   components: {
     IntroductionMI1,
+    ApearInMedia,
     MIServices,
     CustomerGallary,
     MISponsor,

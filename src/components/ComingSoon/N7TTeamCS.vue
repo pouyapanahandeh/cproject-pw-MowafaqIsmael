da82e8db944116cc7b124b4eab4dcf454bb61042
pouyapanahandeh <panahandehpooya@gmail.com>
2022-07-24 15:16:14 +0200

    onfire change, grid problem fixed

--- src/components/ComingSoon/N7TTeamCS.vue
+++ src/components/ComingSoon/N7TTeamCS.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="full-width full-height" id="overlay">
     <div class="text-white in-center tag-line-text">
-      <span class="text-h2" style="background-color: black"
-        >Coming
-        <span class="text-black" style="background-color: white"
-          >soon</span
-        ></span
+      <span class="text-h2 cs-font"
+        >Coming <span class="text-black">soon</span></span
       >
       <br />
-      <span class="text-h3">you can, you will!</span>
+      <span class="text-h3 cs-font">you can, you will!</span>
       <br />
-      <span class="text-h6">by N7T team</span>
+      <span class="text-h6 cs-font">by N7T team</span>
     </div>
   </div>
 
   <div class="full-width full-height">
     <div class="row">
-      <div class="col-6 col-md-4 hide-in-mobile">
+      <div class="col-6 col-md-4 col-sm-4" id="vid-1">
         <video
           src="../../assets/imgs/CSVideo/vid-1.mp4"
           autoplay
@@ -24,7 +21,7 @@
           muted
         ></video>
       </div>
-      <div class="col-6 col-md-4 hide-in-mobile">
+      <div class="col-6 col-md-4 col-sm-4" id="vid-2">
         <video
           src="../../assets/imgs/CSVideo/vid-2.mp4"
           autoplay
@@ -32,10 +29,10 @@
           muted
         ></video>
       </div>
-      <div class="col-6 col-md-4 col-xs-12 full-height">
+      <div class="col-6 col-md-4 col-sm-4 col-xs-12">
         <video
-          src="../../assets/imgs/CSVideo/vid-4.mp4"
-          class="full-width"
+          src="../../assets/imgs/CSVideo/vid-3.mp4"
+          id="vid-3"
           autoplay
           loop
           muted
@@ -56,9 +53,17 @@
   z-index: 2;
 }
 
-@media (max-width: 768px) {
-  .hide-in-mobile {
+@media screen and (max-width: 599px) {
+  #vid-1 {
     display: none;
+  }
+
+  #vid-2 {
+    display: none;
+  }
+
+  #vid-3 {
+    width: 100vh;
   }
 }
 </style>
